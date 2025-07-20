e80533e4875c6ba1e311507c9a17fc4da84288ca
Luis Kreihsl <luis.kreihsl@gmx.at>
2025-07-20 20:19:58 +0200

    feat(vacs-client): add deep link & single instance plugin

--- vacs-client/src/lib.rs
+++ vacs-client/src/lib.rs
@@ -1,3 +1,4 @@
+mod auth;
 mod signaling;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -9,7 +10,17 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{:?}", argv);
+        }))
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            use tauri_plugin_deep_link::DeepLinkExt;
+            app.deep_link().register_all()?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
